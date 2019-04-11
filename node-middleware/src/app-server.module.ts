726bc0e7e4d54fa2ba01dbb950ba4b1c9cfe6bcb
HaoDaWang <1306036576@qq.com>
2019-04-11 20:51:59 +0800

    [Update] inject APP_BASE_HREF

--- node-middleware/src/app-server.module.ts
+++ node-middleware/src/app-server.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core'
 import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader'
 import {ServerModule} from '@angular/platform-server'
+import {APP_BASE_HREF} from '@angular/common'
 
 import {AppModule} from '../../client/src/app/app.module'
 import {AppComponent} from '../../client/src/app/app.component'
@@ -13,6 +14,9 @@ import {AppComponent} from '../../client/src/app/app.component'
   ],
   bootstrap:[
     AppComponent
+  ],
+  providers:[
+    {provide:APP_BASE_HREF, useValue:'/'}
   ]
 })
 export class AppServerModule {}
